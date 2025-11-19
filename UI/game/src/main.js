6babb3bf965bdf1e06fa994f7564ebeeaec3e1d2
naveenrenold <v-nrenold@microsoft.com>
2025-11-19 12:19:37 +0530

    javascript

--- UI/game/src/main.js
+++ UI/game/src/main.js
@@ -1,5 +1,21 @@
-// document.getElementById("root").innerHTML = `
-// <div>
-// Dynamic insertion
-// </div>
-// `;
+var a = 10;
+var list = "";
+var names = ["Naveen", "Renold", "JJ"];
+for (var i = 1; i < 4; i++) {
+  list += `<tr>
+<td>${i}</td>
+<td>${names[i - 1]}</td>
+</tr>`;
+}
+
+document.querySelector("#root").innerHTML = `
+<div>
+<table>
+<tr>
+<th>Id</th>
+<th>Name</th>
+</tr>
+${list}
+</table>
+</div>
+`;
